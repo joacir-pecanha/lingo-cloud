218c90fcc5fce9be842717b103f3a9b5cb2529cb
Joacir Peçanha <joacir99@yahoo.com.br>
2026-06-15 12:21:43 -0300

    feat: implement gamification system with XP, levels, and streaks, and add results dashboard

--- App.tsx
+++ App.tsx
@@ -5,18 +5,21 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { AuthProvider } from './src/context/AuthContext';
 import { CourseProvider } from './src/context/CourseContext';
+import { GamificationProvider } from './src/context/GamificationContext';
 import { RootNavigator } from './src/navigation/RootNavigator';
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <AuthProvider>
-        <CourseProvider>
-          <NavigationContainer>
-            <StatusBar style="light" />
-            <RootNavigator />
-          </NavigationContainer>
-        </CourseProvider>
+        <GamificationProvider>
+          <CourseProvider>
+            <NavigationContainer>
+              <StatusBar style="light" />
+              <RootNavigator />
+            </NavigationContainer>
+          </CourseProvider>
+        </GamificationProvider>
       </AuthProvider>
     </SafeAreaProvider>
   );
